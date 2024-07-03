28f59c0a59fa9876410a292de816af79f87baa37
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-07-03 23:26:27 +0530

    functionality to delet mail

--- Frontend/src/pages/Bin.jsx
+++ Frontend/src/pages/Bin.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import EmailCard from '../components/EmailCard';
+import axios from 'axios';
 
 const Bin = () => {
-  return (
-    <div>
-      <h1>Welcom to BIn</h1>
-    </div>
-  )
-}
+  const [emails, setEmails] = useState([]);
+  
+  const token = JSON.parse(localStorage.getItem('token'))
 
-export default Bin
+  useEffect(() => {
+    fetchEmails();
+  }, []);
+
+  const fetchEmails = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/mail/bin',{
+        headers :{
+          'Authorization' :`Bearer ${token}`
+        }
+    });
+      setEmails(response.data.emails);
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error fetching emails:', error);
+    }
+  };
+
+  
+  
+
+  return (
+    <div className="p-6 min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Sentbox</h1>
+      {emails?.map((email) => (
+        <EmailCard
+          key={email._id}
+          email={email}
+          showStar={false}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Bin;
