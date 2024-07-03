ab3deebac64c14a9dfcf4e1eee4e79db33bdc1c2
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-07-03 23:27:56 +0530

    component to see sent emails

--- Frontend/src/pages/Sentbox.jsx
+++ Frontend/src/pages/Sentbox.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import EmailCard from '../components/EmailCard';
+import axios from 'axios';
 
 const Sentbox = () => {
-  return (
-    <div>
-      <h1>Welcome to sentbox</h1>
-    </div>
-  )
-}
+  const [emails, setEmails] = useState([]);
+  
+  const token = JSON.parse(localStorage.getItem('token'))
 
-export default Sentbox
+  useEffect(() => {
+    fetchEmails();
+  }, []);
+
+  const fetchEmails = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/mail/sentMails',{
+        headers :{
+          'Authorization' :`Bearer ${token}`
+        }
+    });
+    
+      setEmails(response.data.emails);
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error fetching emails:', error);
+    }
+  };
+
+  const handleStarClick = async (id) => {
+    try {
+      await axios.post(`http://localhost:4000/api/mail/${id}/star`,{},{
+        headers :{
+          'Authorization' :`Bearer ${token}`
+        }
+    });
+      fetchEmails(); 
+    } catch (error) {
+      console.error('Error starring email:', error);
+    }
+  };
+
+  const handleDeleteClick = async (id) => {
+    try {
+      await axios.delete(`http://localhost:4000/api/mail/${id}`,{
+        headers :{
+          'Authorization' :`Bearer ${token}`
+        }
+    });
+      fetchEmails();
+    } catch (error) {
+      console.error('Error deleting email:', error);
+    }
+  };
+  console.log(emails)
+  return (
+    <div className="p-6 min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Sentbox</h1>
+      {emails?.map((email) => (
+        <EmailCard
+          key={email._id}
+          email={email}
+          onStarClick={handleStarClick}
+          onDeleteClick={handleDeleteClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Sentbox;
